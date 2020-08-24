c02e2fa149e8a9466d3ac899f8a6ca4162f515e0
soft <2559811312@qq.com>
2020-08-24 15:55:36 +0800

    内置分页views

---
+++ views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+L = []
+for i in range(999):
+    L.append(i)
+
+def index(request):
+    current_page = request.GET.get('p')
+
+    paginator = Paginator(L, 10)
+    # per_page: 每页显示条目数量
+    # count:    数据总个数
+    # num_pages:总页数
+    # page_range:总页数的索引范围，如: (1,10),(1,200)
+    # page:     page对象
+    try:
+        posts = paginator.page(current_page)
+        # has_next              是否有下一页
+        # next_page_number      下一页页码
+        # has_previous          是否有上一页
+        # previous_page_number  上一页页码
+        # object_list           分页之后的数据列表
+        # number                当前页
+        # paginator             paginator对象
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'index.html', {'posts': posts})
